9db143318b260e300d4832a14c76f5aedce1af57
Pdpe <pierredepeyster@Pierres-MacBook-Pro.local>
2017-09-10 22:57:32 -0400

    prob6 update

--- 6_LenghtConversion.py
+++ 6_LenghtConversion.py
@@ -5,3 +5,12 @@ inches = input("Enter number of inches: ")
 
 totalInches = ((63360 * miles) + (36 * yards) + (12 * feet) + (inches))
 totalMeters = totalInches/39.37
+
+kilometers = int(totalMeters/1000)
+meters = int(totalMeters-(kilometers*1000))
+centimeters = (totalMeters-(kilometers*1000)-(meters))*100
+
+print 'Metric length:'
+print kilometers, 'kilometers'
+print meters, 'meters'
+print round(centimeters,2), 'centimeters'
