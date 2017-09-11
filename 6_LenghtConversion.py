5d27c23e31067a2a009b52d2c7b147245dd40001
Pdpe <root@Pierres-MacBook-Pro.local>
2017-09-10 21:47:59 -0400

    added prob.1 files and updated prob.6

--- 6_LenghtConversion.py
+++ 6_LenghtConversion.py
@@ -1,3 +1,7 @@
 miles = input("Enter number of miles: ")
 yards = input("Enter number of yards: ")
-feet = input("Enter number of inches: ")
+feet = input("Enter number of feet: ")
+inches = input("Enter number of inches: ")
+
+totalInches = ((63360 * miles) + (36 * yards) + (12 * feet) + (inches))
+totalMeters = totalInches/39.37
