5c49dd07e21cf34d7effeba63e5f5823cf597edd
Pdpe <pierredepeyster@Pierres-MacBook-Pro.local>
2017-09-12 22:07:53 -0400

    created folder for chapter 2 files

--- 4_UnitPrice.py
+++
@@ -1,8 +0,0 @@
-price = input('Enter price of item: ' )
-print('Enter weight of item in pounds and ounces seperately')
-pounds = input('Enter pounds: ')
-ounces = input('Enter ounces: ')
-
-lbsToOz = (pounds * 16)
-priceperoz = price / ( lbsToOz + ounces )
-print 'Price per ounce: ${}'.format(priceperoz)
