2db57e861d7c941bb0087365384314f747230d8b
Pierre De <29659459+Pdpe@users.noreply.github.com>
2018-09-20 23:55:05 -0400

    Update automate_nodecore.sh

--- automate_nodecore.sh
+++ automate_nodecore.sh
@@ -4,7 +4,7 @@ for session in $(screen -ls | grep -o '[0-9]*\.nodecore'); do screen -S "${sessi
 echo "Initializing miners..."
 for i in `seq 0 7`;
 do
-        screen -S nodecore -dm bash -c "./nodecore_pow_cuda_1 -o 74.79.107.169:8501 -u VBYTXuj2Q8meiNXHmWiEzZWPd2zXSu -d $i"
+        screen -S nodecore -dm bash -c "./nodecore_pow_cuda_1 -o 74.79.107.169:8501 -u V9EJwrn9BBe4G4p2BoUbtYQbY3ZEoQ -d $i"
         echo it $i done.
 done
 echo "All miners initialized."
